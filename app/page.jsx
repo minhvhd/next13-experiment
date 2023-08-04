63f7799e41eaad5eeeba65b71a6698838b51efea
minh.vo <dev.minhvo@gmail.com>
2023-08-04 15:58:15 +0700

    feat: setup tailwindcss

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-red-900">Hello Next 13</h1>
+    </main>
+  )
+}
